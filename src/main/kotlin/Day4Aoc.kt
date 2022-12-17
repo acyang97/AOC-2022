23e06ce876ded13c97ffa362ebf8aa49e53d68b0
acyang97 <acyang97@gmail.com>
2022-12-17 21:23:14 +0800

    Complete day 4 part 2

--- src/main/kotlin/Day4Aoc.kt
+++ src/main/kotlin/Day4Aoc.kt
@@ -4,7 +4,7 @@ import java.io.FileReader
 import java.io.IOException
 
 fun main() {
-    part1()
+    part2()
 }
 
 fun part1() {
@@ -35,5 +35,26 @@ fun part1() {
 }
 
 fun part2() {
-
+    var total = 0
+    try {
+        val file = File("/Users/chunyang/Desktop/indeed/revision/AOC/src/main/kotlin/input-4.txt");
+        BufferedReader(FileReader(file)).use { br ->
+            var line: String?
+            while (br.readLine().also { line = it } != null) {
+                var x = line!!.split(",")
+                val first = x[0]
+                val second = x[1]
+                val firstLeft = first.split("-")[0].toInt();
+                val firstRight = first.split("-")[1].toInt();
+                val secondLeft = second.split("-")[0].toInt();
+                val secondRight = second.split("-")[1].toInt();
+                if (!(firstRight < secondLeft || secondRight < firstLeft)) {
+                    total++
+                }
+            }
+        }
+        println("total is $total")
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
 }
