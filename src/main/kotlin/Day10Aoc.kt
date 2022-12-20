2cf2334f2d330df192b34854cf59e84ab1635f76
acyang97 <acyang97@gmail.com>
2022-12-20 21:26:00 +0800

    Complete day 10 part 2

--- src/main/kotlin/Day10Aoc.kt
+++ src/main/kotlin/Day10Aoc.kt
@@ -4,7 +4,7 @@ import java.io.FileReader
 import java.io.IOException
 
 fun main() {
-    day10Part1()
+    day10Part2()
 }
 
 fun processInputDay10(): MutableList<String> {
@@ -21,6 +21,67 @@ fun processInputDay10(): MutableList<String> {
         e.printStackTrace()
     }
     return ls;
+}
+
+fun day10Part2() {
+    val input = processInputDay10();
+    var currentCycle = 1;
+    var spritePositionStart = 0
+    var spritePositionEnd = 2
+    // at the start of cycle 1, at position 1
+    var stringToBuild = "#"
+    var valuesAtStart = mutableListOf<Int>(1);
+    for (line in input) {
+        val splitted = line.split(" ")
+        val command = splitted[0];
+        val value: Int;
+        if (command == "addx") {
+            value = splitted[1].toInt()
+            println("value is $value")
+            currentCycle++;
+            // sprite hasnt move, dont need do anything
+            // add to the string
+            if (((currentCycle - 1) % 40) != spritePositionStart &&
+                ((currentCycle - 1) % 40) != spritePositionEnd &&
+                ((currentCycle - 1) % 40) != ((spritePositionStart+spritePositionEnd)/2)%40
+            ) {
+                stringToBuild += "."
+            } else {
+                stringToBuild += "#"
+            }
+            println("Current cycle is $currentCycle, spritePositionStart is $spritePositionStart, spritePositionEnd is $spritePositionEnd")
+            currentCycle++;
+            spritePositionStart = (spritePositionStart + value + 40) % 40
+            spritePositionEnd = (spritePositionEnd + value + 40) % 40
+            if (((currentCycle - 1) % 40) != spritePositionStart &&
+                ((currentCycle - 1) % 40) != spritePositionEnd &&
+                ((currentCycle - 1) % 40) != ((spritePositionStart+spritePositionEnd)/2)%40
+            ) {
+                stringToBuild += "."
+            } else {
+                stringToBuild += "#"
+            }
+            println("Current cycle is $currentCycle, spritePositionStart is $spritePositionStart, spritePositionEnd is $spritePositionEnd")
+
+        } else { // noop
+            currentCycle++;
+            if (((currentCycle - 1) % 40) != spritePositionStart &&
+                ((currentCycle - 1) % 40) != spritePositionEnd &&
+                ((currentCycle - 1) % 40) != ((spritePositionStart+spritePositionEnd)/2)%40
+            ) {
+                stringToBuild += "."
+            } else {
+                stringToBuild += "#"
+            }
+            println("Current cycle is $currentCycle, spritePositionStart is $spritePositionStart, spritePositionEnd is $spritePositionEnd")
+
+        }
+    }
+//    var ans = 0;
+    for (cycle in 0..currentCycle step 40) {
+        println(stringToBuild.substring(cycle, cycle+ 40))
+    }
+//    println("Answer is $ans")
 }
 
 fun day10Part1() {
@@ -51,4 +112,3 @@ fun day10Part1() {
     }
     println("Answer is $ans")
 }
-
