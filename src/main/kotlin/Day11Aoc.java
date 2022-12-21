eabed453b25527ce5a4ff74d3cd36312078938de
acyang97 <acyang97@gmail.com>
2022-12-21 21:03:27 +0800

    Change file name

--- src/main/kotlin/Day11Aoc.java
+++ src/main/kotlin/Day11Aoc.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
 
-public class Day10Aoc {
+public class Day11Aoc {
     static List<Monkey> listOfMonkeys = new ArrayList<>();
 
     public static void main(String[] args) {
